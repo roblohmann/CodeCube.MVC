d4fbb470169ef5e15ca60dd295779ac27106b4e1
Rob Lohmann <roblohman@hotmail.com>
2019-05-19 16:05:21 +0200

    added code to get assembly version as string.

--- CodeCube.MVC.AspNetCore/HtmlHelpers/General.cs
+++ CodeCube.MVC.AspNetCore/HtmlHelpers/General.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace CodeCube.MVC.AspNetCore.HtmlHelpers
@@ -13,6 +14,41 @@ namespace CodeCube.MVC.AspNetCore.HtmlHelpers
         public static string CurrentMachine(this HtmlHelper helper)
         {
             return $"{Environment.MachineName}";
+        }
+
+        /// <summary>
+        /// Get the version from the current assembly as string.
+        /// </summary>
+        /// <returns>The assembly version as string.</returns>
+        public static string CurrentVersion(this HtmlHelper helper)
+        {
+            try
+            {
+                var version = Assembly.GetExecutingAssembly().GetName().Version;
+                return string.Format("{0}.{1}.{2}.{3}", version.Major, version.Minor, version.Build.ToString("00"), version.Revision.ToString("00"));
+            }
+            catch (Exception)
+            {
+                return "?.?.?.?";
+            }
+        }
+
+        /// <summary>
+        /// Get the version for the specified assembly as string.
+        /// </summary>
+        /// <param name="assemblyName">The assembly to get the version for.</param>
+        /// <returns>The assembly version as string.</returns>
+        public static string CurrentVersion(this HtmlHelper helper, String assemblyName)
+        {
+            try
+            {
+                var version = Assembly.Load(assemblyName).GetName().Version;
+                return string.Format("{0}.{1}.{2}.{3}", version.Major, version.Minor, version.Build.ToString("00"), version.Revision.ToString("00"));
+            }
+            catch (Exception)
+            {
+                return "?.?.?.?";
+            }
         }
     }
 }
